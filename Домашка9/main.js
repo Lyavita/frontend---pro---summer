c719c6d7f515339f55dde726ad98aa57858372c4
Lyavita <lyavita@gmail.com>
2021-07-10 17:27:52 +0300

    черновик2

---
+++ Домашка9/main.js
@@ -0,0 +1,19 @@
+// let count = 0;
+// let elem = getNodeListByAttr('[data-id]');
+// let sum = document.querySelector('#summ');
+
+// elem.forEach(function (el, index, list) {
+//    count += +replaceLetterInNumber(el.innerHTML.split('')).join('');
+// });
+
+
+// console.log(sum) ;
+// console.log(sum.innerHTML);
+// sum.innerHTML += ' ' + count;
+// console.log(sum.innerHTML);
+
+// alert(summ.textContent);
+// alert(summ.textContent + "ddd");
+
+
+// console.log(count);
