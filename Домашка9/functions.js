8b7f1c52ddf683e5beec843b6cd9b8d210092ba6
Lyavita <lyavita@gmail.com>
2021-07-10 17:47:26 +0300

    v.1.0

--- Домашка9/functions.js
+++ Домашка9/functions.js
@@ -1,28 +1,14 @@
 let getNodeListByAttr = attr => document.querySelectorAll(attr);
 
-function replaceLetterByNumber(number) {
-for (let i = 0; i < number.length; i++) {
-    if (number[i] === 'B') {
-        number[i] = '000000000';
-     } else if (number[i] === 'M') {
-        number[i] = '000000';
-     }
-   }
-return number;
+function replaceLetterInNumber(number) {
+    for (let i = 0; i < number.length; i++) {
+        if (number[i] === 'B') {
+            number[i] = '000000000';
+        } else if (number[i] === 'M') {
+            number[i] = '000000';
+        }
+    }
+    return number;
 };
-
-let count = 0;
-console.log(getNodeListByAttr('[data-id]'));
-
-let elem = getNodeListByAttr('[data-id]');
-
-elem.forEach(function (el, index, list) {
-    
-    console.log(replaceLetterByNumber(el.innerHTML.split('')).join(''));
-    count += +replaceLetterByNumber(el.innerHTML.split('')).join('');
-    
-})
-
-console.log(count);
 
 
--- Домашка9/main.js
+++ Домашка9/main.js
@@ -1,19 +1,6 @@
-// let count = 0;
-// let elem = getNodeListByAttr('[data-id]');
-// let sum = document.querySelector('#summ');
-
-// elem.forEach(function (el, index, list) {
-//    count += +replaceLetterInNumber(el.innerHTML.split('')).join('');
-// });
-
-
-// console.log(sum) ;
-// console.log(sum.innerHTML);
-// sum.innerHTML += ' ' + count;
-// console.log(sum.innerHTML);
-
-// alert(summ.textContent);
-// alert(summ.textContent + "ddd");
-
-
-// console.log(count);
+let sum = 0;
+let elements = getNodeListByAttr('[data-id]');
+elements.forEach(function(element) {
+    sum += +replaceLetterInNumber(element.innerHTML.split('')).join('');
+});
+document.querySelector('#summ').innerHTML += ' ' + sum;
