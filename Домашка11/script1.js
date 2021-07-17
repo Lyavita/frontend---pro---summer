98e01dd18140f1e73d9be6c8cd140c3a857bd88a
Lyavita <lyavita@gmail.com>
2021-07-17 17:23:14 +0300

    Домашка11

---
+++ Домашка11/script1.js
@@ -0,0 +1,34 @@
+function putToList(task) {
+    let nextTask = document.createElement("li");
+    let delTask = document.createElement("button");
+    nextTask.setAttribute("class", "color-will");
+    nextTask.textContent = task.value;
+    list.appendChild(nextTask);
+    nextTask.appendChild(delTask);
+    task.value = "";
+    nextTask.addEventListener('click', () => gggt(nextTask));
+    delTask.addEventListener('click', () => removeTask(nextTask));
+    // delTask.onclick = function () {
+    //     delTask.parentElement.delete();
+    // }
+
+}
+
+function removeTask(n) {
+    list.removeChild(n);
+}
+function gggt(d) {
+    d.classList.toggle('color-will');
+    d.classList.toggle('color-did');
+}
+
+
+// nextTask.classList.toggle('color1');
+let lii = document.querySelectorAll('li');
+lii.forEach(liii => console.log(liii));
+
+
+
+let oneMoreTask = document.querySelector('#input-list');
+let list = document.querySelector('#todo-list');
+console.log(btn);
