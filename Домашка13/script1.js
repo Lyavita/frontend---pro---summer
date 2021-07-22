c57ffe745342064d9e9141120928630be20a45ef
Lyavita <lyavita@gmail.com>
2021-07-22 15:40:22 +0300

    Домашка13

---
+++ Домашка13/script1.js
@@ -0,0 +1,31 @@
+const calc = createCalculator(10);
+
+function createCalculator(b) {
+    return {
+        sum: function (q) {
+            b = q + b
+            return b;
+        },
+        mult: function (q) {
+            b = q * b;
+            return b;
+        },
+        sub: function (q) {
+            b = b - q;
+            return b;
+        },
+        div: function (q) {
+            b = b / q;
+            return b;
+        },
+        set: function (q) {
+            return q;
+        },
+    }
+}
+
+console.log(calc.sum(5));
+console.log(calc.mult(10));
+console.log(calc.sub(40));
+console.log(calc.div(10));
+console.log(calc.set(100));
