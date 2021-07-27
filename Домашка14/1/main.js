b283e86bc395683b5f9e03b417b414ed4c74f960
Lyavita <lyavita@gmail.com>
2021-07-27 14:30:47 +0300

    11

--- Домашка14/1/main.js
+++ Домашка14/1/main.js
@@ -1,20 +1,3 @@
-document.addEventListener("DOMContentLoaded", changeFontSize);
-
-window.addEventListener("load", function () {
-    let btnDelAttr = document.getElementById("removeAttrBtn");
-    btnDelAttr.addEventListener("click", removeAttribute, false);
-
-    let greenBtn = document.getElementById("setGreenBtn");
-    greenBtn.addEventListener("click", paintGreen, true);
-
-    let oneMoreRowBtn = document.getElementById("addRowBtn");
-    oneMoreRowBtn.addEventListener("click", addRow);
-
-    let delRowBtn = document.getElementById("removeSecondRowBtn");
-    delRowBtn.addEventListener("click", removeRow);
-});
-
-
 
 
 
