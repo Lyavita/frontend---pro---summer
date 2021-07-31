73d2a5499d5e8551debfca166f99ce0d13c37308
Lyavita <lyavita@gmail.com>
2021-07-31 16:05:52 +0300

    убрала комментарий

--- Домашка13/script1.js
+++ Домашка13/script1.js
@@ -1,43 +1,32 @@
-// const calc = createCalculator(10);
+const calc = createCalculator(10);
 
-// function createCalculator(b) {
-//     return {
-//         sum: function (q) {
-//             b = q + b
-//             return b;
-//         },
-//         mult: function (q) {
-//             b = q * b;
-//             return b;
-//         },
-//         sub: function (q) {
-//             b = b - q;
-//             return b;
-//         },
-//         div: function (q) {
-//             b = b / q;
-//             return b;
-//         },
-//         set: function (q) {
-//             return q;
-//         },
-//     }
-// }
-
-// console.log(calc.sum(5));
-// console.log(calc.mult(10));
-// console.log(calc.sub(40));
-// console.log(calc.div(10));
-// console.log(calc.set(100));
-
-function say667() {
-    // Local variable that ends up within closure
-    var num = 666;
-    var sayAlert = function () { alert(num); }
-    num++;
-    return sayAlert;
+function createCalculator(b) {
+    return {
+        sum: function (q) {
+            b = q + b
+            return b;
+        },
+        mult: function (q) {
+            b = q * b;
+            return b;
+        },
+        sub: function (q) {
+            b = b - q;
+            return b;
+        },
+        div: function (q) {
+            b = b / q;
+            return b;
+        },
+        set: function (q) {
+            b = q;
+            return q;
+        },
+    }
 }
 
-var sayNumber = say667();
-alert(sayNumber());
-alert(sayNumber()); alert(sayNumber());
+console.log(calc.sum(5));
+console.log(calc.mult(10));
+console.log(calc.sub(40));
+console.log(calc.div(10));
+console.log(calc.set(100));
