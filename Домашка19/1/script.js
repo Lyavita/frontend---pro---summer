ad77ffe09e4bd5af7864f5433e627fde4b0f2d8e
Lyavita <lyavita@gmail.com>
2021-08-15 12:29:05 +0300

    v1.0.3

--- Домашка19/1/script.js
+++ Домашка19/1/script.js
@@ -3,6 +3,7 @@ const minuts = document.getElementById("minuts");
 const seconds = document.getElementById("seconds");
 
 const day = document.getElementById("day");
+const dayWeek = document.getElementById("day-week");
 const month = document.getElementById("month");
 const year = document.getElementById("year");
 
@@ -13,10 +14,11 @@ function setTimeN() {
     minuts.textContent = `${(now.getMinutes() < 10) ? '0' + now.getMinutes() : now.getMinutes()}`;
     seconds.textContent = `${(now.getSeconds() < 10) ? '0' + now.getSeconds() : now.getSeconds()}`;
 
-    let days = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+    let days = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
     let monthes = ["Янв", "Фев", "Мар", "Апр", "Май", "Июн", "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек"];
 
-    day.textContent = `${days[now.getDay()]}`;
+    day.textContent = `${now.getDate()}`;
+    dayWeek.textContent = `${days[now.getDay()]}`;
     month.textContent = `${monthes[now.getMonth()]}`;
     year.textContent = `${now.getFullYear()}`;
 
