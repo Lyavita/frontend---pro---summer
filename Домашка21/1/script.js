85a8cb90d55071bbec25bb1c7cbbbac85b4b7c1f
Lyavita <lyavita@gmail.com>
2021-08-25 14:27:10 +0300

    v1.0.4

--- Домашка21/1/script.js
+++ Домашка21/1/script.js
@@ -1,46 +1,41 @@
 const urlToDos = "https://jsonplaceholder.typicode.com/todos";
 const list = document.querySelector("#todo-list");
 const btnLoadToDos = document.querySelector("#btn-load-todos");
-let countToDoPosition = trigger();
-btnLoadToDos.addEventListener("click", () => countToDoPosition(urlToDos));
+btnLoadToDos.addEventListener("click", () => onLoadToDosClick());
 
-function trigger() {
-  let clicks = 0;
-  return function(url) {
-    ++clicks;
-    onLoadToDosClick(url, clicks);
-  }
+fetch("https://jsonplaceholder.typicode.com/todos")
+  .then(res => res.json())
+  .then(function (data) {
+    globalMassTodos = data;
+  })
+
+function onLoadToDosClick() {
+  let elem = globalMassTodos.shift();
+  putToList(elem);
 }
+       
+function putToList(dataFromServer) {
+  let dataId = dataFromServer.id;
+  let title = dataFromServer.title;
+  let status = dataFromServer.completed;
 
-function onLoadToDosClick(url, clicks) { 
-  fetch(url)
-    .then(res => res.json())
-    .then(data => putToList(data, clicks))
+  let nextTask = document.createElement("li");
+  let delTask = document.createElement("button");
+  if (status) {
+      nextTask.setAttribute("class", "color-did");
+      delTask.textContent = "Удалить задание (задание выполнено)";
+  } else {
+      nextTask.setAttribute("class", "color-will");
+      delTask.textContent = "Удалить задание (задание еще не выполнено)";
   }
-      
-function putToList(dataFromServer, pos) {
-    let dataId = dataFromServer.id;
-    let title = dataFromServer[pos].title;
-    let status = dataFromServer[pos].completed;
-    let nextTask = document.createElement("li");
-    let delTask = document.createElement("button");
-    if (status) {
-        nextTask.setAttribute("class", "color-did");
-        delTask.setAttribute("class", "color-did");
-        delTask.textContent = "Удалить задание (задание выполнено)";
-    } else {
-        nextTask.setAttribute("class", "color-will");
-        delTask.setAttribute("class", "color-will");
-        delTask.textContent = "Удалить задание (задание еще не выполнено)";
-    }
-    nextTask.setAttribute("data-id", `${dataId}`);
-    nextTask.textContent = title;
-    nextTask.classList.add("li-task");
-    nextTask.addEventListener("click", () => switchTaskStatus(nextTask, delTask));
-    list.appendChild(nextTask);
-    delTask.classList.add("button-del");
-    delTask.addEventListener("click", () => removeTask(nextTask, delTask));
-    list.appendChild(delTask);
+  nextTask.setAttribute("data-id", `${dataId}`);
+  nextTask.textContent = title;
+  nextTask.classList.add("li-task");
+  nextTask.addEventListener("click", () => switchTaskStatus(nextTask, delTask));
+  nextTask.appendChild(delTask);
+  list.appendChild(nextTask);
+  delTask.classList.add("button-del");
+  delTask.addEventListener("click", () => removeTask(nextTask, delTask));
 }
 
 function removeTask(task, btn) {
@@ -51,10 +46,9 @@ function removeTask(task, btn) {
 function switchTaskStatus(task, btn) {
   task.classList.toggle("color-will");
   task.classList.toggle("color-did");
-  btn.classList.toggle("color-will");
-  btn.classList.toggle("color-did");
   if (task.classList.contains("color-did")) {
       btn.textContent = "Удалить задание (задание выполнено)";
+  } else {
+      btn.textContent = "Удалить задание (задание еще не выполнено)";
   }
 }
-
--- Домашка21/2/script.js
+++ Домашка21/2/script.js
@@ -10,29 +10,27 @@ function onLoadToDosClick(url) {
 }
         
 function putToList(dataFromServer) {
-    let dataId = dataFromServer.id;
-    let title = dataFromServer.title;
-    let status = dataFromServer.completed;
+  let dataId = dataFromServer.id;
+  let title = dataFromServer.title;
+  let status = dataFromServer.completed;
 
-    let nextTask = document.createElement("li");
-    let delTask = document.createElement("button");
-    if (status) {
-        nextTask.setAttribute("class", "color-did");
-        delTask.setAttribute("class", "color-did");
-        delTask.textContent = "Удалить задание (задание выполнено)";
-    } else {
-        nextTask.setAttribute("class", "color-will");
-        delTask.setAttribute("class", "color-will");
-        delTask.textContent = "Удалить задание (задание еще не выполнено)";
-    }
-    nextTask.setAttribute("data-id", `${dataId}`);
-    nextTask.textContent = title;
-    nextTask.classList.add("li-task");
-    nextTask.addEventListener("click", () => switchTaskStatus(nextTask, delTask));
-    list.appendChild(nextTask);
-    delTask.classList.add("button-del");
-    delTask.addEventListener("click", () => removeTask(nextTask, delTask));
-    list.appendChild(delTask);
+  let nextTask = document.createElement("li");
+  let delTask = document.createElement("button");
+  if (status) {
+      nextTask.setAttribute("class", "color-did");
+      delTask.textContent = "Удалить задание (задание выполнено)";
+  } else {
+      nextTask.setAttribute("class", "color-will");
+      delTask.textContent = "Удалить задание (задание еще не выполнено)";
+  }
+  nextTask.setAttribute("data-id", `${dataId}`);
+  nextTask.textContent = title;
+  nextTask.classList.add("li-task");
+  nextTask.addEventListener("click", () => switchTaskStatus(nextTask, delTask));
+  nextTask.appendChild(delTask);
+  list.appendChild(nextTask);
+  delTask.classList.add("button-del");
+  delTask.addEventListener("click", () => removeTask(nextTask, delTask));
 }
 
 function removeTask(task, btn) {
@@ -43,9 +41,9 @@ function removeTask(task, btn) {
 function switchTaskStatus(task, btn) {
   task.classList.toggle("color-will");
   task.classList.toggle("color-did");
-  btn.classList.toggle("color-will");
-  btn.classList.toggle("color-did");
   if (task.classList.contains("color-did")) {
-      btn.textContent = "Удалить задание (задание выполнено)";
+    btn.textContent = "Удалить задание (задание выполнено)";
+  }  else {
+    btn.textContent = "Удалить задание (задание еще не выполнено)";
   }
 }
