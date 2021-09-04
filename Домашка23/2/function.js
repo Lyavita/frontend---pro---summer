62e23973f9290aca3bd73ddb0716712f338f640f
Lyavita <lyavita@gmail.com>
2021-09-04 10:13:24 +0300

    v1.0

---
+++ Домашка23/2/function.js
@@ -0,0 +1,10 @@
+const countTotal = () => {return {total: 100}};
+console.log(countTotal());
+
+const result = `Today is ${new Date}`;
+console.log(result);
+
+let a = 2, b = 3;
+console.log(a, b);
+[a, b] = [b, a];
+console.log(a, b);
