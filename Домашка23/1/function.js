f4ae8325d12e7deceb501cef0d48d9a696f07d78
Lyavita <lyavita@gmail.com>
2021-09-05 16:29:15 +0300

    v1.0.1

--- Домашка23/1/function.js
+++ Домашка23/1/function.js
@@ -1,77 +1,84 @@
 class Calculator {
 
   static el = (element) => element.charAt(0) === "#" ? document.querySelector(element) : document.querySelectorAll(element);
-  
-  static VIEWER = Calculator.el("#viewer");
-  static EQUALS = Calculator.el("#equals");
-  static NUMS = Calculator.el(".num");
-  static OPS = Calculator.el(".ops");
-  static #THE_NUM = "";
-  static #OLD_NUM = "";
-  static RESULT_NUM;
-  static OPERATOR;
 
-  constructor() { }
+  #theNum;
+  #oldNum;
+
+  constructor() {
+  this.viewer = Calculator.el("#viewer");
+  this.equals = Calculator.el("#equals");
+  this.nums = Calculator.el(".num");
+  this.ops = Calculator.el(".ops");
+  this.#theNum = "";
+  this.#oldNum = "";
+  this.resultNum;
+  this.operator;
+  }
   
   setClick() {
-    for (let i = 0, l = Calculator.NUMS.length; i < l; i++) {
-        Calculator.NUMS[i].addEventListener("click", this.setNum);
+    console.log(this)
+    for (let i = 0, l = this.nums.length; i < l; i++) {
+        this.nums[i].addEventListener("click", this.setNum.bind(this));
     }
-    for (let i = 0, l = Calculator.OPS.length; i < l; i++) {
-        Calculator.OPS[i].addEventListener("click", this.moveNum);
+    for (let i = 0, l = this.ops.length; i < l; i++) {
+        this.ops[i].addEventListener("click", this.moveNum.bind(this));
     }
-    Calculator.EQUALS.addEventListener("click", this.displayNum);
-    Calculator.el("#clear").addEventListener("click", this.clearAll);
+    this.equals.addEventListener("click", this.displayNum.bind(this));
+    Calculator.el("#clear").addEventListener("click", this.clearAll.bind(this));
     Calculator.el("#reset").addEventListener("click", () => window.location = window.location);
   }
 
-  setNum() {
-    Calculator.RESULT_NUM ? (Calculator.#THE_NUM = this.getAttribute("data-num"), Calculator.RESULT_NUM = "") : (Calculator.#THE_NUM += this.getAttribute("data-num"));
-    Calculator.VIEWER.innerHTML = Calculator.#THE_NUM;
+  setNum(event) {
+    console.log(this)
+    this.resultNum ? (this.#theNum = event.target.getAttribute("data-num"), this.resultNum= "") : (this.#theNum += event.target.getAttribute("data-num"));
+    this.viewer.innerHTML = this.#theNum;
   };
 
-  moveNum() {
-    Calculator.#OLD_NUM = Calculator.#THE_NUM;
-    Calculator.#THE_NUM = "";
-    Calculator.OPERATOR = this.getAttribute("data-ops");
-    Calculator.EQUALS.setAttribute("data-result", "");
+  moveNum(event) {
+    console.log(this)
+    this.#oldNum = this.#theNum;
+    this.#theNum = "";
+    this.operator = event.target.getAttribute("data-ops");
+    this.equals.setAttribute("data-result", "");
   };
 
   displayNum() {
-    Calculator.#OLD_NUM = parseFloat(Calculator.#OLD_NUM);
-    Calculator.#THE_NUM = parseFloat(Calculator.#THE_NUM);
-    switch (Calculator.OPERATOR) {
+    console.log(this)
+    this.#oldNum = parseFloat(this.#oldNum);
+    this.#theNum = parseFloat(this.#theNum);
+    switch (this.operator) {
       case "plus":
-          Calculator.RESULT_NUM = Calculator.#OLD_NUM + Calculator.#THE_NUM;
+          this.resultNum= this.#oldNum + this.#theNum;
           break;
       case "minus":
-          Calculator.RESULT_NUM = Calculator.#OLD_NUM - Calculator.#THE_NUM;
+          this.resultNum= this.#oldNum - this.#theNum;
           break;
       case "times":
-          Calculator.RESULT_NUM = Calculator.OLD_NUM * Calculator.THE_NUM;
+          this.resultNum= this.oldNum * this.theNum;
           break;
       case "divided by":
-          Calculator.RESULT_NUM = Calculator.#OLD_NUM / Calculator.#THE_NUM;
+          this.resultNum= this.#oldNum / this.#theNum;
           break;
       default:
-          Calculator.RESULT_NUM = Calculator.#THE_NUM;
+          this.resultNum= this.#theNum;
     }
 
-    if (!isFinite(Calculator.RESULT_NUM)) {
-      isNaN(Calculator.RESULT_NUM) ? Calculator.RESULT_NUM = "You broke it!" : (Calculator.RESULT_NUM = "Look at what you've done", Calculator.el('#calculator').classList.add("broken"), Calculator.el('#reset').classList.add("show"))
+    if (!isFinite(this.resultNum)) {
+      isNaN(this.resultNum) ? this.resultNum= "You broke it!" : (this.resultNum= "Look at what you've done", Calculator.el('#calculator').classList.add("broken"), Calculator.el('#reset').classList.add("show"))
     }
 
-    Calculator.VIEWER.innerHTML = Calculator.RESULT_NUM;
-    Calculator.EQUALS.setAttribute("data-result", Calculator.RESULT_NUM);
-    Calculator.#OLD_NUM = 0;
-    Calculator.#THE_NUM = Calculator.RESULT_NUM;
+    this.viewer.innerHTML = this.resultNum;
+    this.equals.setAttribute("data-result", this.resultNum);
+    this.#oldNum = 0;
+    this.#theNum = this.resultNum;
   };
 
   clearAll() {
-    Calculator.#OLD_NUM = "";
-    Calculator.#THE_NUM = "";
-    Calculator.VIEWER.innerHTML = "0";
-    Calculator.EQUALS.setAttribute("data-result", Calculator.RESULT_NUM);
+    this.#oldNum = "";
+    this.#theNum = "";
+    this.viewer.innerHTML = "0";
+    this.equals.setAttribute("data-result", this.resultNum);
   };
 }
 
