0031f792f4995bc94d2344a901fb57f5b43a33bb
Lyavita <lyavita@gmail.com>
2021-10-06 00:26:40 +0300

    format config

--- FinalProcopy/webpack.config.js
+++ FinalProcopy/webpack.config.js
@@ -2,25 +2,22 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development', 
-    entry: {
-        main: path.resolve(__dirname, 'src/app.js'),
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-      filename: '[name].[contenthash].js',
-      clean: true,
-        
+  mode: 'development', 
+  entry: {
+    main: path.resolve(__dirname, 'src/app.js'),
   },
-    
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+  },
   devtool: 'inline-source-map',
   devServer: {
     static: path.resolve(__dirname, 'dist'),
     open: true,
     hot: true,
-   
-    },
-    module: {
+  },
+  module: {
     rules: [
       //css
       { test: /\.css$/, use: ['style-loader', 'css-loader'] },
@@ -28,7 +25,7 @@ module.exports = {
       { test: /\.(svg|ico|png|webp|jpg|gif|jpeg)$/, type: 'asset/resource' },
     ],
   },
-    plugins: [
-        new HtmlWebpackPlugin({template: './src/index.html'})
-      ]
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'})
+  ]
 };
